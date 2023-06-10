d80ce1c1c0ff433575669f84c4080b8761f449f2
Hiền Bùi <buixuanhienmy@gmail.com>
2023-06-10 21:46:47 +0700

    fix-newUser

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -4,31 +4,41 @@ const getHomepage = (req, res) => {
     return res.render('home.ejs');
 };
 
+const displayUser = (req, res) => {
+    connection.query(
+        'SELECT * FROM Users',
+        function (err, results, fields) {
+            if (err) {
+                console.error('Error executing query:', err);
+                res.send(err);
+                return;
+            }
+            res.send(results);
+        }
+    );
+};
+
 const postCreateUser = (req, res) => {
     let {email, myName, address} = req.body;
     console.log(email, myName, address);
 
     connection.query(
-        'SELECT * FROM Users',
-        function (err, results, fields) {
-            res.send(results)
+        'INSERT INTO Users (email, name, city) VALUES (?, ?, ?)',
+        [email, myName, address],
+        function (err, results) {
+            console.log(email, myName, address);
+            if (err) {
+                console.error('Error executing query:', err);
+                res.send(err);
+                return;
+            }
+            res.send('New user created successfully');
         }
-        // 'INSERT INTO Users (email, name, city) VALUES (?,?,?)',
-        // [email, myName, address],
-        // function (err, results) {
-          //   console.log(email, myName, address);
-            // if (err) {
-              //   console.error('Error executing query:', err);
-                // res.send(err);
-                // return;
-            // }
-             //res.send('New user created successfully');
-        // }
     );
 };
 
-
 module.exports = {
     getHomepage,
-    postCreateUser
+    postCreateUser,
+    displayUser
 };
--- src/routers/web.js
+++ src/routers/web.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const {getHomepage,postCreateUser} = require("../controllers/homeController")
-//router Method('/route', handler);
+const {getHomepage, displayUser,postCreateUser } = require("../controllers/homeController");
+
 router.get('/', getHomepage);
 router.post('/create-user', postCreateUser);
+router.get('/display-user', displayUser);
 
 module.exports = router;
